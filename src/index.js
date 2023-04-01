02e27d79d9ea8b6c3b6b5171de388f29cc3ab0d9
Israel Salinas <israel.salinas.m@gmail.com>
2023-04-01 03:23:19 -0600

    Added router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { FaTabletAlt } from "react-icons/fa";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RegisterForm from "./routes/RegisterForm";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/register",
+    element: <RegisterForm />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
