9ca3b4b9284e98c19e067c8f2e4899ad9c4a1c1c
SHALEM MONSERRAT SOLIS LAGUNAS <smonserrat@totalplay.corp>
2023-04-01 02:12:58 -0600

    feat(styles): general styles

--- src/components/DescriptiveCard.js
+++ src/components/DescriptiveCard.js
@@ -1,17 +1,18 @@
 import { FaRegCheckCircle } from "react-icons/fa";
+
 const DescriptiveCard = (props) => {
   const { image, content } = props;
   return (
     <div className="col">
-      <div className="feature-card card h-100 bg-transparent shadow-none border-0">
+      <div className="feature-card card h-100 bg-transparent shadow-none border-0 bullet">
         <div className="card-body">
-          <img src={image} alt="" />
-          <ul>
+          <img src={image} alt="" height="70%" width="70%"/>
+          <ul className="mt-4">
             {content.map((item) => {
               return (
-                <li>
-                  <FaRegCheckCircle />
-                  {item}
+                <li className="d-flex flex-row align-items-baseline">
+                  <FaRegCheckCircle  size={18} color="fa4d09"/>
+                  <span className="fs-5 ms-3">{item}</span>
                 </li>
               );
             })}
