9b391ef26acaa44ad08d37a71fa54aaae2a1a01c
Israel Salinas <israel.salinas.m@gmail.com>
2023-04-01 02:21:38 -0600

    fixed image size

--- src/components/DescriptiveCard.js
+++ src/components/DescriptiveCard.js
@@ -5,13 +5,13 @@ const DescriptiveCard = (props) => {
   return (
     <div className="col">
       <div className="feature-card card h-100 bg-transparent shadow-none border-0 bullet">
-        <div className="card-body">
-          <img src={image} alt="" height="70%" width="70%"/>
+        <div className="card-body d-flex flex-column justify-content-center align-items-center">
+          <img src={image} alt="" className="w-50" />
           <ul className="mt-4">
             {content.map((item) => {
               return (
                 <li className="d-flex flex-row align-items-baseline">
-                  <FaRegCheckCircle  size={18} color="fa4d09"/>
+                  <FaRegCheckCircle size={18} color="fa4d09" />
                   <span className="fs-5 ms-3">{item}</span>
                 </li>
               );
